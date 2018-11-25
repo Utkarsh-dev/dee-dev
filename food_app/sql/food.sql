dad56675395a5227084eb918f5f93349573a1967
Utkarsh-dev <43725999+Utkarsh-dev@users.noreply.github.com>
2018-11-25 11:33:30 +0530

    Update food.sql

--- food_app/sql/food.sql
+++ food_app/sql/food.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
+-- version 4.5.4.1deb2ubuntu2.1
+-- http://www.phpmyadmin.net
 --
--- Host: 127.0.0.1
--- Generation Time: Mar 30, 2017 at 05:22 PM
--- Server version: 10.1.21-MariaDB
--- PHP Version: 5.6.30
+-- Host: localhost
+-- Generation Time: Nov 25, 2018 at 11:32 AM
+-- Server version: 5.7.24-0ubuntu0.16.04.1
+-- PHP Version: 7.0.32-0ubuntu0.16.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `items` (
   `id` int(11) NOT NULL,
   `name` varchar(20) NOT NULL,
+  `category_id` int(11) NOT NULL,
   `price` int(11) NOT NULL,
   `deleted` tinyint(4) NOT NULL DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -37,12 +38,27 @@ CREATE TABLE `items` (
 -- Dumping data for table `items`
 --
 
-INSERT INTO `items` (`id`, `name`, `price`, `deleted`) VALUES
-(1, 'Item 1', 25, 1),
-(2, 'Item 2', 45, 0),
-(3, 'Item 3', 20, 0),
-(4, 'Item 4', 15, 1),
-(5, 'Item 5', 20, 0);
+INSERT INTO `items` (`id`, `name`, `category_id`, `price`, `deleted`) VALUES
+(10, 'Saada Chana', 1, 45, 0),
+(11, 'Chana Masala', 1, 45, 0),
+(12, 'Finger Chips', 1, 45, 0),
+(13, 'Alloo Fry', 1, 45, 0),
+(14, 'Chilli Potato', 1, 90, 0),
+(15, 'Dry Papad', 1, 20, 0),
+(16, 'Masala Papad', 1, 30, 0),
+(17, 'Alloo Pakoda', 1, 45, 0),
+(18, 'Pyaz Pakoda', 1, 45, 0),
+(19, 'Mix Pakoda', 1, 45, 0),
+(20, 'Veg Maggie', 1, 50, 0),
+(21, 'Veg Manchurian', 1, 110, 0),
+(22, 'Egg Paneer Bhujiya', 2, 120, 0),
+(23, 'Egg Bhujiya', 2, 45, 0),
+(24, 'Egg Omlette', 2, 45, 0),
+(25, 'Egg Maggie', 2, 60, 0),
+(26, 'Chicken Fry Half', 2, 150, 0),
+(27, 'Chicken Fry Full', 2, 300, 0),
+(28, 'Chicken Fry Qtr', 2, 80, 0),
+(29, 'Soda Bottle', 3, 20, 0);
 
 -- --------------------------------------------------------
 
@@ -51,28 +67,23 @@ INSERT INTO `items` (`id`, `name`, `price`, `deleted`) VALUES
 --
 
 CREATE TABLE `orders` (
-  `id` int(11) NOT NULL,
-  `customer_id` int(11) NOT NULL,
-  `address` varchar(300) NOT NULL,
-  `description` varchar(300) NOT NULL,
-  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `payment_type` varchar(16) NOT NULL DEFAULT 'Wallet',
-  `total` int(11) NOT NULL,
-  `status` varchar(25) NOT NULL DEFAULT 'Yet to be delivered',
-  `deleted` tinyint(4) NOT NULL DEFAULT '0'
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `table_id` varchar(20) NOT NULL,
+  `waiter_id` int(11) NOT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `total_bill` int(11) NOT NULL,
+  `status` varchar(20) NOT NULL,
+  `comments` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `orders`
 --
 
-INSERT INTO `orders` (`id`, `customer_id`, `address`, `description`, `date`, `payment_type`, `total`, `status`, `deleted`) VALUES
-(1, 2, 'Address 2', 'Sample Description 1', '2017-03-28 17:32:41', 'Wallet', 150, 'Yet to be delivered', 0),
-(2, 2, 'New address 2', '', '2017-03-28 17:43:05', 'Wallet', 130, 'Cancelled by Customer', 1),
-(3, 3, 'Address 3', 'Sample Description 2', '2017-03-28 19:49:33', 'Cash On Delivery', 130, 'Yet to be delivered', 0),
-(4, 3, 'Address 3', '', '2017-03-28 19:52:01', 'Cash On Delivery', 130, 'Cancelled by Customer', 1),
-(5, 3, 'New Address 3', '', '2017-03-28 20:47:28', 'Wallet', 285, 'Paused', 0),
-(6, 3, 'New Address 3', '', '2017-03-30 00:43:31', 'Wallet', 325, 'Cancelled by Customer', 1);
+INSERT INTO `orders` (`id`, `table_id`, `waiter_id`, `time`, `total_bill`, `status`, `comments`) VALUES
+(1, 't1', 1, '2018-11-24 18:17:06', 175, 'PREPARING', ''),
+(2, 't2', 2, '2018-11-24 18:18:07', 225, 'PREPARING', ''),
+(3, 't3', 1, '2018-11-24 19:33:45', 145, 'PREPARING', '');
 
 -- --------------------------------------------------------
 
@@ -81,32 +92,25 @@ INSERT INTO `orders` (`id`, `customer_id`, `address`, `description`, `date`, `pa
 --
 
 CREATE TABLE `order_details` (
-  `id` int(11) NOT NULL,
+  `id` bigint(20) UNSIGNED NOT NULL,
   `order_id` int(11) NOT NULL,
-  `item_id` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL,
-  `price` int(11) NOT NULL
+  `item` varchar(50) NOT NULL,
+  `price` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `order_details`
 --
 
-INSERT INTO `order_details` (`id`, `order_id`, `item_id`, `quantity`, `price`) VALUES
-(1, 1, 2, 2, 90),
-(2, 1, 3, 3, 60),
-(3, 2, 2, 2, 90),
-(4, 2, 3, 2, 40),
-(5, 3, 2, 2, 90),
-(6, 3, 3, 2, 40),
-(7, 4, 2, 2, 90),
-(8, 4, 3, 2, 40),
-(9, 5, 2, 5, 225),
-(10, 5, 3, 2, 40),
-(11, 5, 5, 1, 20),
-(12, 6, 2, 5, 225),
-(13, 6, 3, 3, 60),
-(14, 6, 5, 2, 40);
+INSERT INTO `order_details` (`id`, `order_id`, `item`, `price`, `quantity`) VALUES
+(1, 1, '10', 90, 2),
+(2, 1, '24', 45, 1),
+(3, 1, '29', 40, 2),
+(4, 2, '12', 135, 3),
+(5, 2, '14', 90, 1),
+(6, 3, '20', 100, 2),
+(7, 3, '17', 45, 1);
 
 -- --------------------------------------------------------
 
@@ -168,8 +172,8 @@ CREATE TABLE `users` (
   `name` varchar(15) NOT NULL,
   `username` varchar(10) NOT NULL,
   `password` varchar(16) NOT NULL,
-  `email` varchar(35) NULL,
-  `address` varchar(300) NULL,
+  `email` varchar(35) DEFAULT NULL,
+  `address` varchar(300) DEFAULT NULL,
   `contact` bigint(11) NOT NULL,
   `verified` tinyint(1) NOT NULL DEFAULT '0',
   `deleted` tinyint(4) NOT NULL DEFAULT '0'
@@ -189,49 +193,22 @@ INSERT INTO `users` (`id`, `role`, `name`, `username`, `password`, `email`, `add
 -- --------------------------------------------------------
 
 --
--- Table structure for table `wallet`
+-- Table structure for table `waiters`
 --
 
-CREATE TABLE `wallet` (
-  `id` int(11) NOT NULL,
-  `customer_id` int(11) NOT NULL
+CREATE TABLE `waiters` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `name` varchar(20) NOT NULL,
+  `working` tinyint(1) NOT NULL DEFAULT '1'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `wallet`
+-- Dumping data for table `waiters`
 --
 
-INSERT INTO `wallet` (`id`, `customer_id`) VALUES
-(1, 1),
-(2, 2),
-(3, 3),
-(4, 4),
-(5, 5);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `wallet_details`
---
-
-CREATE TABLE `wallet_details` (
-  `id` int(11) NOT NULL,
-  `wallet_id` int(11) NOT NULL,
-  `number` varchar(16) NOT NULL,
-  `cvv` int(3) NOT NULL,
-  `balance` int(11) NOT NULL DEFAULT '2000'
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `wallet_details`
---
-
-INSERT INTO `wallet_details` (`id`, `wallet_id`, `number`, `cvv`, `balance`) VALUES
-(1, 1, '6155247490533921', 983, 3430),
-(2, 2, '1887587142382050', 772, 1850),
-(3, 3, '4595809639046830', 532, 1585),
-(4, 4, '5475856443351234', 521, 2000),
-(5, 5, '9076633115663264', 229, 2000);
+INSERT INTO `waiters` (`id`, `name`, `working`) VALUES
+(1, 'Nepali', 1),
+(2, 'XYZ', 1);
 
 --
 -- Indexes for dumped tables
@@ -249,18 +226,16 @@ ALTER TABLE `items`
 -- Indexes for table `orders`
 --
 ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`),
-  ADD KEY `customer_id` (`customer_id`);
+  ADD KEY `waiter_id` (`waiter_id`);
 
 --
 -- Indexes for table `order_details`
 --
 ALTER TABLE `order_details`
-  ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `id` (`id`),
-  ADD KEY `item_id` (`item_id`),
-  ADD KEY `order_id` (`order_id`);
+  ADD KEY `order_id` (`order_id`),
+  ADD KEY `item` (`item`);
 
 --
 -- Indexes for table `tickets`
@@ -286,19 +261,9 @@ ALTER TABLE `users`
   ADD UNIQUE KEY `id` (`id`);
 
 --
--- Indexes for table `wallet`
+-- Indexes for table `waiters`
 --
-ALTER TABLE `wallet`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `customer_id` (`customer_id`),
-  ADD UNIQUE KEY `id` (`id`);
-
---
--- Indexes for table `wallet_details`
---
-ALTER TABLE `wallet_details`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `wallet_id` (`wallet_id`),
+ALTER TABLE `waiters`
   ADD UNIQUE KEY `id` (`id`);
 
 --
@@ -309,17 +274,17 @@ ALTER TABLE `wallet_details`
 -- AUTO_INCREMENT for table `items`
 --
 ALTER TABLE `items`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 --
 -- AUTO_INCREMENT for table `orders`
 --
 ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- AUTO_INCREMENT for table `order_details`
 --
 ALTER TABLE `order_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 --
 -- AUTO_INCREMENT for table `tickets`
 --
@@ -336,31 +301,13 @@ ALTER TABLE `ticket_details`
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 --
--- AUTO_INCREMENT for table `wallet`
+-- AUTO_INCREMENT for table `waiters`
 --
-ALTER TABLE `wallet`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
---
--- AUTO_INCREMENT for table `wallet_details`
---
-ALTER TABLE `wallet_details`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `waiters`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `order_details`
---
-ALTER TABLE `order_details`
-  ADD CONSTRAINT `order_details_ibfk_1` FOREIGN KEY (`item_id`) REFERENCES `items` (`id`),
-  ADD CONSTRAINT `order_details_ibfk_2` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`);
 
 --
 -- Constraints for table `tickets`
@@ -374,18 +321,6 @@ ALTER TABLE `tickets`
 ALTER TABLE `ticket_details`
   ADD CONSTRAINT `ticket_details_ibfk_1` FOREIGN KEY (`ticket_id`) REFERENCES `tickets` (`id`),
   ADD CONSTRAINT `ticket_details_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `wallet`
---
-ALTER TABLE `wallet`
-  ADD CONSTRAINT `wallet_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `users` (`id`);
-
---
--- Constraints for table `wallet_details`
---
-ALTER TABLE `wallet_details`
-  ADD CONSTRAINT `wallet_details_ibfk_1` FOREIGN KEY (`wallet_id`) REFERENCES `wallet` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
