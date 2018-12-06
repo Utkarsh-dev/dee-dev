4af93d42d90ab7fe7d738de0d6040513c9339804
Utkarsh <utkrshsingh.29@gmail.com>
2018-12-06 13:22:33 +0530

    Python exercises

---
+++ Python/sum.py
@@ -0,0 +1,19 @@
+lst = [-1, 1, 2,4,5, 7, 8]
+sum = 6
+first = 0
+last = len(lst) - 1
+print(lst)
+out = []
+while (first < last):
+	if( lst[first] + lst[last] > sum ):
+		last -= 1
+	elif(lst[first] + lst[last] <  sum):
+		first += 1
+	elif(lst[first] + lst[last] ==  sum):
+		out.append([lst[first],lst[last]])
+		last -= 1
+		first += 1
+	else:
+	   break
+
+print("Pairs of values with sum = ",sum,out)
